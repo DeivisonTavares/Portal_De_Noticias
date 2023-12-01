26ccff821d40de19828d3beb9f4899b239dcf694
DeivisonTavares <deivisontavaresdev@gmail.com>
2023-12-01 15:51:46 -0300

    .

---
+++ site/Script/mudar_paginas.js
@@ -0,0 +1,19 @@
+function Ir_Tela_Inicial(params) {
+    location.replace('../../site/tela_inicial/index.html');
+}
+
+function Ir_Tela_Locais(params) {
+    location.replace('../../site/tela_locais/tela_locais.html');
+}
+
+function Ir_Tela_Internacional(params) {
+    location.replace('../../site/tela_internacional/tela_internacional.html');
+}
+
+function Ir_Tela_Esporte(params) {
+    location.replace('../../site/tela_esportes/tela_esporte.html');
+}
+
+function Ir_Tela_Musica(params) {
+    location.replace('../../site/tela_musicas/tela_musicas.html');
+}
